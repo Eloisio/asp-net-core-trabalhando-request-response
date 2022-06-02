b44599670f604d0cd248a0b13d801d43af55e6c7
Cleyson Lima <cleysonph@gmail.com>
2022-06-02 19:55:28 -0300

    02:04 - respondendo templates

--- Controllers/TesteController.cs
+++ Controllers/TesteController.cs
@@ -7,7 +7,13 @@ public class TesteController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        // var viewResult = new ViewResult();
+        // viewResult.ViewName = "Index";
+        // return viewResult;
+        // var partialViewResult = new PartialViewResult();
+        // partialViewResult.ViewName = "Index";
+        // return partialViewResult;
+        return PartialView();
     }
 
 }
